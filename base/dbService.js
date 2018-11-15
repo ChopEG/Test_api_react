8a7bc9a0d34a8f31181d1d2517d9ba9041b5b9e3
ChopEG <chop77.eg@gmail.com>
2018-11-15 18:31:55 +0200

    revision project service

--- base/dbService.js
+++ base/dbService.js
@@ -2,21 +2,11 @@
 const { connection } = require('../db');
 const models = require('../models');
 
-const NEGATIVE_RE = /^-/;
-
 const defaultSort = 'id';
 const defaultLimit = 100;
 const defaultSkip = 0;
 
 const getId = (obj) => obj._id;
-const normalizeSort = (sort) => {
-  const direction = sort.startsWith('-') ? -1 : 1;
-  const field = sort.replace(NEGATIVE_RE, '');
-  return {
-    [field]: direction,
-  };
-};
-
 const create = (Model, data) => Model.create(data);
 
 module.exports = {
@@ -27,5 +17,4 @@ module.exports = {
   models,
   getId,
   create,
-  normalizeSort,
 };
--- services/projectService.js
+++ services/projectService.js
@@ -2,19 +2,20 @@ const { dbService } = require('../base');
 const { curry } = require('../base/utils');
 
 const DATA_TYPE = 'Project';
-const defaultSort = 'name';
-const { defaultLimit, defaultSkip, models } = dbService;
+const { defaultSort, defaultLimit, defaultSkip, models } = dbService;
 const { Project } = models;
 
 const create = curry(dbService.create, Project);
-const findAll = (
-  { sort = defaultSort, limit = defaultLimit, skip = defaultSkip },
+const findAll = ({
+  sort = defaultSort,
+  limit = defaultLimit,
+  skip = defaultSkip,
   populate = ['contractors'],
-) =>
+}) =>
   Project.find()
     .limit(limit)
     .skip(skip)
-    .sort(dbService.normalizeSort(sort))
+    .sort(sort)
     .populate(...populate);
 
 module.exports = {
